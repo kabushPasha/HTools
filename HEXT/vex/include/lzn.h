f27e484d8493b3451bd9521fdf9e3009a567ac9f
kabushPasha <kabushpavel@gmail.com>
2017-10-30 18:55:54 +0300

    Noises fixes

--- HEXT/vex/include/lzn.h
+++ HEXT/vex/include/lzn.h
@@ -15,176 +15,173 @@ aap - perlin noise
 
 CURL NOISE
 cpn - curl perlin noise
-
+can - alligator
+cxn - smiplex
+csn - sparse convultion
+con - original perlin
 */
 
 	// SMIPLE TURBULENT NOISES
 // ORIGINAL PERLIN NOISE
-float opfn(vector pos;int turb;float rough;float atten){	
+float ofn(vector pos;int turb;float rough;float atten){	
 	float noise = onoise(pos, turb, rough, atten);	
 	return noise;}
-float opfn(vector pos;int turb;float rough){	
+float ofn(vector pos;int turb;float rough){	
 	float noise = onoise(pos, turb, rough,1);
 	return noise;}
-float opfn(vector pos;int turb){	
+float ofn(vector pos;int turb){	
 	float noise = onoise(pos, turb, 0.5,1);	
 	return noise;}
-float opfn(vector pos){	
+float ofn(vector pos){	
 	float noise = onoise(pos, 3, 0.5,1);	
 	return noise;}
-float opfn1(vector pos){	
-	float noise = onoise(pos, 1, 0.5,1);	
-	return noise;}
 	
-vector opvn(vector pos;int turb;float rough;float atten){	
+vector ovn(vector pos;int turb;float rough;float atten){	
 	vector noise = onoise(pos, turb, rough, atten);	
 	return noise;}
-vector opvn(vector pos;int turb;float rough){	
+vector ovn(vector pos;int turb;float rough){	
 	vector noise = onoise(pos, turb, rough,1);	
 	return noise;}
-vector opvn(vector pos;int turb){	
+vector ovn(vector pos;int turb){	
 	vector noise = onoise(pos, turb, 0.5,1);	
 	return noise;}
-vector opvn(vector pos){	
+vector ovn(vector pos){	
 	vector noise = onoise(pos, 3, 0.5,1);	
 	return noise;}
-vector opvn1(vector pos){	
-	vector noise = onoise(pos, 1, 0.5,1);	
-	return noise;}
-	
+
 // ALLIGATOR NOISE
-float alfn(vector pos;int turb;float rough;float atten){	
+float afn(vector pos;int turb;float rough;float atten){	
 	float noise = anoise(pos, turb, rough, atten);	
 	return noise;}
-float alfn(vector pos;int turb;float rough){	
+float afn(vector pos;int turb;float rough){	
 	float noise = anoise(pos, turb, rough,1);	
 	return noise;}
-float alfn(vector pos;int turb){	
+float afn(vector pos;int turb){	
 	float noise = anoise(pos, turb, 0.5,1);	
 	return noise;}
-float alfn(vector pos){	
+float afn(vector pos){	
 	float noise = anoise(pos, 3, 0.5,1);	
 	return noise;}
 
-vector alfn(vector pos;int turb;float rough;float atten){	
+vector afn(vector pos;int turb;float rough;float atten){	
 	vector noise = anoise(pos, turb, rough, atten);	
 	return noise;}
-vector alfn(vector pos;int turb;float rough){	
+vector afn(vector pos;int turb;float rough){	
 	vector noise = anoise(pos, turb, rough,1);	
 	return noise;}
-vector alfn(vector pos;int turb){	
+vector afn(vector pos;int turb){	
 	vector noise = anoise(pos, turb, 0.5,1);	
 	return noise;}
-vector alfn(vector pos){	
+vector afn(vector pos){	
 	vector noise = anoise(pos, 3, 0.5,1);	
 	return noise;}
 
 // SPARSE CONVULTION NOISE
-float scfn(vector pos;int turb;float rough;float atten){	
+float sfn(vector pos;int turb;float rough;float atten){	
 	float noise = snoise(pos, turb, rough, atten);	
 	return noise;}
-float scfn(vector pos;int turb;float rough){	
+float sfn(vector pos;int turb;float rough){	
 	float noise = snoise(pos, turb, rough,1);	
 	return noise;}
-float scfn(vector pos;int turb){	
+float sfn(vector pos;int turb){	
 	float noise = snoise(pos, turb, 0.5,1);	
 	return noise;}
-float scfn(vector pos){	
+float sfn(vector pos){	
 	float noise = snoise(pos, 3, 0.5,1);	
 	return noise;}
 
-vector scvn(vector pos;int turb;float rough;float atten){	
+vector svn(vector pos;int turb;float rough;float atten){	
 	vector noise = snoise(pos, turb, rough, atten);	
 	return noise;}
-vector scvn(vector pos;int turb;float rough){	
+vector svn(vector pos;int turb;float rough){	
 	vector noise = snoise(pos, turb, rough,1);	
 	return noise;}
-vector scvn(vector pos;int turb){	
+vector svn(vector pos;int turb){	
 	vector noise = snoise(pos, turb, 0.5,1);	
 	return noise;}
-vector scvn(vector pos){	
+vector svn(vector pos){	
 	vector noise = snoise(pos, 3, 0.5,1);	
 	return noise;}
 
 	// VOP TURBULENT NOISES
 // SIMPLEX NOISE	
-float sxfn(vector pos;int turb;float rough;float atten){	
+float xfn(vector pos;int turb;float rough;float atten){	
 	float noise = vop_simplexNoiseVF(pos, turb, 1, rough, atten);
 	return noise;}
-float sxfn(vector pos;int turb;float rough){	
+float xfn(vector pos;int turb;float rough){	
 	float noise = vop_simplexNoiseVF(pos, turb,1, rough,1);
 	return noise;}
-float sxfn(vector pos;int turb){	
+float xfn(vector pos;int turb){	
 	float noise = vop_simplexNoiseVF(pos, turb,1, 0.5,1);	
 	return noise;}
-float sxfn(vector pos){	
+float xfn(vector pos){	
 	float noise = vop_simplexNoiseVF(pos, 3,1, 0.5,1);	
 	return noise;}
 	
-vector sxvn(vector pos;int turb;float rough;float atten){	
+vector xvn(vector pos;int turb;float rough;float atten){	
 	vector noise = vop_simplexNoiseVF(pos, turb,1, rough, atten);	
 	return noise;}
-vector sxvn(vector pos;int turb;float rough){	
+vector xvn(vector pos;int turb;float rough){	
 	vector noise = vop_simplexNoiseVV(pos, turb,1, rough,1);	
 	return noise;}
-vector sxvn(vector pos;int turb){	
+vector xvn(vector pos;int turb){	
 	vector noise = vop_simplexNoiseVV(pos, turb,1, 0.5,1);	
 	return noise;}
-vector sxvn(vector pos){	
+vector xvn(vector pos){	
 	vector noise = vop_simplexNoiseVV(pos, 3,1, 0.5,1);	
 	return noise;}
 	
 // PERLIN NOISE	
-float pnfn(vector pos;int turb;float rough;float atten){	
+float pfn(vector pos;int turb;float rough;float atten){	
 	float noise = vop_perlinNoiseVF(pos, turb, 1, rough, atten);
 	return noise;}
-float pnfn(vector pos;int turb;float rough){	
+float pfn(vector pos;int turb;float rough){	
 	float noise = vop_perlinNoiseVF(pos, turb,1, rough,1);
 	return noise;}
-float pnfn(vector pos;int turb){	
+float pfn(vector pos;int turb){	
 	float noise = vop_perlinNoiseVF(pos, turb,1, 0.5,1);	
 	return noise;}
-float pnfn(vector pos){	
+float pfn(vector pos){	
 	float noise = vop_perlinNoiseVF(pos, 3,1, 0.5,1);	
 	return noise;}
 	
-vector pnvn(vector pos;int turb;float rough;float atten){	
+vector pvn(vector pos;int turb;float rough;float atten){	
 	vector noise = vop_perlinNoiseVV(pos, turb,1, rough, atten);	
 	return noise;}
-vector pnvn(vector pos;int turb;float rough){	
+vector pvn(vector pos;int turb;float rough){	
 	vector noise = vop_perlinNoiseVV(pos, turb,1, rough,1);	
 	return noise;}
-vector pnvn(vector pos;int turb){	
+vector pvn(vector pos;int turb){	
 	vector noise = vop_perlinNoiseVV(pos, turb,1, 0.5,1);	
 	return noise;}
-vector pnvn(vector pos){	
+vector pvn(vector pos){	
 	vector noise = vop_perlinNoiseVV(pos, 3,1, 0.5,1);	
 	return noise;}	
 	
 // CORRECTED PERLIN NOISE	
-float cpfn(vector pos;int turb;float rough;float atten){	
+float cfn(vector pos;int turb;float rough;float atten){	
 	float noise = vop_correctperlinNoiseVF(pos, turb, 1, rough, atten);
 	return noise;}
-float cpfn(vector pos;int turb;float rough){	
+float cfn(vector pos;int turb;float rough){	
 	float noise = vop_correctperlinNoiseVF(pos, turb,1, rough,1);
 	return noise;}
-float cpfn(vector pos;int turb){	
+float cfn(vector pos;int turb){	
 	float noise = vop_correctperlinNoiseVF(pos, turb,1, 0.5,1);	
 	return noise;}
-float cpfn(vector pos){	
+float cfn(vector pos){	
 	float noise = vop_correctperlinNoiseVF(pos, 3,1, 0.5,1);	
 	return noise;}
 	
-vector cpvn(vector pos;int turb;float rough;float atten){	
+vector cvn(vector pos;int turb;float rough;float atten){	
 	vector noise = vop_correctperlinNoiseVV(pos, turb,1, rough, atten);	
 	return noise;}
-vector cpvn(vector pos;int turb;float rough){	
+vector cvn(vector pos;int turb;float rough){	
 	vector noise = vop_correctperlinNoiseVV(pos, turb,1, rough,1);	
 	return noise;}
-vector cpvn(vector pos;int turb){	
+vector cvn(vector pos;int turb){	
 	vector noise = vop_correctperlinNoiseVV(pos, turb,1, 0.5,1);	
 	return noise;}
-vector cpvn(vector pos){	
+vector cvn(vector pos){	
 	vector noise = vop_correctperlinNoiseVV(pos, 3,1, 0.5,1);	
 	return noise;}		
 	
@@ -192,126 +189,126 @@ vector cpvn(vector pos){
 	// AA NOISES
 //PERLIN
 // 3D 
-float aapfn(vector pos;int turb;float rough){	
+float apfn(vector pos;int turb;float rough){	
 	float noise = vop_fbmNoiseFV(pos,rough,turb,"noise");	
 	return noise;}
-float aapfn(vector pos;int turb){	
+float apfn(vector pos;int turb){	
 	float noise = vop_fbmNoiseFV(pos, 0.5,turb,"noise");	
 	return noise;}
-float aapfn(vector pos){	
+float apfn(vector pos){	
 	float noise = vop_fbmNoiseFV(pos, 0.5,3,"noise");	
 	return noise;}
 
-vector aapvn(vector pos;int turb;float rough){	
+vector apvn(vector pos;int turb;float rough){	
 	vector noise = vop_fbmNoiseVV(pos,rough,turb,"noise");	
 	return noise;}
-vector aapvn(vector pos;int turb){	
+vector apvn(vector pos;int turb){	
 	vector noise = vop_fbmNoiseVV(pos,0.5, turb,"noise");	
 	return noise;}
-vector aapvn(vector pos){	
+vector apvn(vector pos){	
 	vector noise = vop_fbmNoiseVV(pos, 0.5,3,"noise");	
 	return noise;}
 //1D
-float aapfn(float pos;int turb;float rough){	
+float apfn(float pos;int turb;float rough){	
 	float noise = vop_fbmNoiseFF(pos,rough,turb,"noise");	
 	return noise;}
-float aapfn(float pos;int turb){	
+float apfn(float pos;int turb){	
 	float noise = vop_fbmNoiseFF(pos, 0.5,turb,"noise");	
 	return noise;}
-float aapfn(float pos){	
+float apfn(float pos){	
 	float noise = vop_fbmNoiseFF(pos, 0.5,3,"noise");	
 	return noise;}
 
-vector aapvn(float pos;int turb;float rough){	
+vector apvn(float pos;int turb;float rough){	
 	vector noise = vop_fbmNoiseVF(pos,rough,turb,"noise");	
 	return noise;}
-vector aapvn(float pos;int turb){	
+vector apvn(float pos;int turb){	
 	vector noise = vop_fbmNoiseVF(pos,0.5, turb,"noise");	
 	return noise;}
-vector aapvn(float pos){	
+vector apvn(float pos){	
 	vector noise = vop_fbmNoiseVF(pos, 0.5,3,"noise");	
 	return noise;}
 //4D
-float aapfn(vector4 pos;int turb;float rough){	
+float apfn(vector4 pos;int turb;float rough){	
 	float noise = vop_fbmNoiseFP(pos,rough,turb,"noise");	
 	return noise;}
-float aapfn(vector4 pos;int turb){	
+float apfn(vector4 pos;int turb){	
 	float noise = vop_fbmNoiseFP(pos, 0.5,turb,"noise");	
 	return noise;}
-float aapfn(vector4 pos){	
+float apfn(vector4 pos){	
 	float noise = vop_fbmNoiseFP(pos, 0.5,3,"noise");	
 	return noise;}
 
-vector aapvn(vector4 pos;int turb;float rough){	
+vector apvn(vector4 pos;int turb;float rough){	
 	vector noise = vop_fbmNoiseFP(pos,rough,turb,"noise");	
 	return noise;}
-vector aapvn(vector4 pos;int turb){	
+vector apvn(vector4 pos;int turb){	
 	vector noise = vop_fbmNoiseFP(pos,0.5, turb,"noise");	
 	return noise;}
-vector aapvn(vector4 pos){	
+vector apvn(vector4 pos){	
 	vector noise = vop_fbmNoiseFP(pos, 0.5,3,"noise");	
 	return noise;}	
 
 
 //SIMPLEX 
 //3D
-float aasfn(vector pos;int turb;float rough){	
+float asfn(vector pos;int turb;float rough){	
 	float noise = vop_fbmNoiseFV(pos,rough,turb,"xnoise");	
 	return noise;}
-float aasfn(vector pos;int turb){	
+float asfn(vector pos;int turb){	
 	float noise = vop_fbmNoiseFV(pos, 0.5,turb,"xnoise");	
 	return noise;}
-float aasfn(vector pos){	
+float asfn(vector pos){	
 	float noise = vop_fbmNoiseFV(pos, 0.5,3,"xnoise");	
 	return noise;}
 
-vector aasvn(vector pos;int turb;float rough){	
+vector asvn(vector pos;int turb;float rough){	
 	vector noise = vop_fbmNoiseVV(pos,rough,turb,"xnoise");	
 	return noise;}
-vector aasvn(vector pos;int turb){	
+vector asvn(vector pos;int turb){	
 	vector noise = vop_fbmNoiseVV(pos,0.5, turb,"xnoise");	
 	return noise;}
-vector aasvn(vector pos){	
+vector asvn(vector pos){	
 	vector noise = vop_fbmNoiseVV(pos, 0.5,3,"xnoise");	
 	return noise;}
 //1D
-float aasfn(float pos;int turb;float rough){	
+float asfn(float pos;int turb;float rough){	
 	float noise = vop_fbmNoiseFF(pos,rough,turb,"xnoise");	
 	return noise;}
-float aasfn(float pos;int turb){	
+float asfn(float pos;int turb){	
 	float noise = vop_fbmNoiseFF(pos, 0.5,turb,"xnoise");	
 	return noise;}
-float aasfn(float pos){	
+float asfn(float pos){	
 	float noise = vop_fbmNoiseFF(pos, 0.5,3,"xnoise");	
 	return noise;}
 
-vector aasvn(float pos;int turb;float rough){	
+vector asvn(float pos;int turb;float rough){	
 	vector noise = vop_fbmNoiseVF(pos,rough,turb,"xnoise");	
 	return noise;}
-vector aasvn(float pos;int turb){	
+vector asvn(float pos;int turb){	
 	vector noise = vop_fbmNoiseVF(pos,0.5, turb,"xnoise");	
 	return noise;}
-vector aasvn(float pos){	
+vector asvn(float pos){	
 	vector noise = vop_fbmNoiseVF(pos, 0.5,3,"xnoise");	
 	return noise;}
 //4D
-float aasfn(vector4 pos;int turb;float rough){	
+float asfn(vector4 pos;int turb;float rough){	
 	float noise = vop_fbmNoiseFP(pos,rough,turb,"xnoise");	
 	return noise;}
-float aasfn(vector4 pos;int turb){	
+float asfn(vector4 pos;int turb){	
 	float noise = vop_fbmNoiseFP(pos, 0.5,turb,"xnoise");	
 	return noise;}
-float aasfn(vector4 pos){	
+float asfn(vector4 pos){	
 	float noise = vop_fbmNoiseFP(pos, 0.5,3,"xnoise");	
 	return noise;}
 
-vector aasvn(vector4 pos;int turb;float rough){	
+vector asvn(vector4 pos;int turb;float rough){	
 	vector noise = vop_fbmNoiseFP(pos,rough,turb,"xnoise");	
 	return noise;}
-vector aasvn(vector4 pos;int turb){	
+vector asvn(vector4 pos;int turb){	
 	vector noise = vop_fbmNoiseFP(pos,0.5, turb,"xnoise");	
 	return noise;}
-vector aasvn(vector4 pos){	
+vector asvn(vector4 pos){	
 	vector noise = vop_fbmNoiseFP(pos, 0.5,3,"xnoise");	
 	return noise;}	
 	
@@ -420,10 +417,28 @@ vector cxn(vector4 pos){
 	return curlNoiseVV(pos,3,0.5,1,"xnoise");}	
 
 	
-	
-	
-	
-	
+// WORLEY NOISE	
+float wseed(vector P)	
+{
+	int seed;
+	float f1,f2;
+	wnoise(P, seed, f1, f2);
+	return seed;	
+}
+float wf1(vector P)	
+{
+	int seed;
+	float f1,f2;
+	wnoise(P, seed, f1, f2);
+	return f1;	
+}
+float wf2(vector P)	
+{
+	int seed;
+	float f1,f2;
+	wnoise(P, seed, f1, f2);
+	return f2;	
+}
 	
 	
 	
