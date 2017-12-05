579ccd586e64c04a3dbb2de8b9a42a8da861ea95
kabushPasha <kabushpavel@gmail.com>
2017-12-05 10:46:12 +0300

    small fixed

--- batch/ImgUrl/IMGURL_set.bat
+++ batch/ImgUrl/IMGURL_set.bat
@@ -1,8 +1,15 @@
+echo off
 set exif=%~dp0"exiftool.exe"
 
-echo "Please enter your url"
-set /p new_url="Enter URL: "
-echo %new_url%
-%exif% "-XMP:source=%new_url%" %1 -overwrite_original
+echo Processing image %*
 
-::pause
+::echo "Please enter your url"
+set /p new_url="Enter URL: "
+::echo %new_url%
+%exif% "-XMP:source=%new_url%" "%*" -overwrite_original
+
+set /p new_name="Enter Link Name: "
+::echo %new_name%
+ren "%*" "%new_name%"
+
+pause
