f436ab2da1561cfb178fdae5d1ac6563d06d3614
kabushPasha <kabushpavel@gmail.com>
2017-12-05 12:03:56 +0300

    small imgyrl fixes

--- batch/ImgUrl/IMGURL_set.bat
+++ batch/ImgUrl/IMGURL_set.bat
@@ -1,15 +1,30 @@
+:: add auto add extension
+:: add auto move to folder
+
 echo off
 set exif=%~dp0"exiftool.exe"
 
-echo Processing image %*
+echo Processing image %1
 
 ::echo "Please enter your url"
 set /p new_url="Enter URL: "
-::echo %new_url%
-%exif% "-XMP:source=%new_url%" "%*" -overwrite_original
+%exif% "-XMP:source=%new_url%" %1 -overwrite_original
 
 set /p new_name="Enter Link Name: "
-::echo %new_name%
-ren "%*" "%new_name%"
+if "%new_name%"=="" (
+	echo "i'll keep my name"
+) else (
+	ren %1 "%new_name%"
+)
+
+set /p new_folder="Enter Folder Name: "
+
+if "%new_folder%"=="" (
+echo "I'll stay here"
+) else (
+echo move me to %new_folder%
+echo %cd%\%new_folder%
+ROBOCOPY "%cd%" "%cd%\%new_folder%" "%new_name%" /mov /NFL /NDL /NJH /NJS
+)
 
 pause
