92e6508e72e9e0e325bf02cee13d9096abf75249
kabushPasha <kabushpavel@gmail.com>
2017-12-05 14:51:28 +0300

    fix for target folder

--- batch/ImgUrl/IMGURL_set.bat
+++ batch/ImgUrl/IMGURL_set.bat
@@ -14,17 +14,22 @@ set /p new_name="Enter Link Name: "
 if "%new_name%"=="" (
 	echo "i'll keep my name"
 ) else (
-	ren %1 "%new_name%"
+	ren %1 "%new_name%"%~x1
 )
 
+::ECHO filedrive=%~d1
+::ECHO filepath=%~p1
+::ECHO filename=%~n1
+::ECHO fileextension=%~x1
+
+set dir=%~d1%~p1
 set /p new_folder="Enter Folder Name: "
 
+:: bug will move nothing if new name not set
 if "%new_folder%"=="" (
 echo "I'll stay here"
 ) else (
-echo move me to %new_folder%
-echo %cd%\%new_folder%
-ROBOCOPY "%cd%" "%cd%\%new_folder%" "%new_name%" /mov /NFL /NDL /NJH /NJS
+echo "I'll go away"
+Robocopy.exe  "%dir%." "%dir%%new_folder%" "%new_name%%~x1" /mov /NFL /NDL /NJH /NJS
 )
-
 pause
