b5e7afbeab3b30075f2a3f850b0fda2e438130c5
kabushPasha <kabushpavel@gmail.com>
2017-12-05 10:17:20 +0300

    overwrite image in ImgUrl

--- batch/ImgUrl/IMGURL_set.bat
+++ batch/ImgUrl/IMGURL_set.bat
@@ -3,6 +3,6 @@ set exif=%~dp0"exiftool.exe"
 echo "Please enter your url"
 set /p new_url="Enter URL: "
 echo %new_url%
-%exif% "-XMP:source=%new_url%" %1
+%exif% "-XMP:source=%new_url%" %1 -overwrite_original
 
 ::pause
