4929e22571b1ba46af9ade923a3ca7f600c96fac
kabushPasha <kabushpavel@gmail.com>
2018-01-25 01:44:30 +0300

    Harbor deveopment

--- Harbor/main.py
+++ Harbor/main.py
@@ -1,10 +1,14 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication,QMenu,QAction,QComboBox,QInputDialog,QLineEdit)
+from PyQt5.QtWidgets import (QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication,QMenu,QAction,QComboBox,QInputDialog,QLineEdit,QFileDialog)
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap,QIcon
 from PyQt5 import QtCore
 from glob import glob
 import os
 import subprocess
+import json
+import shutil
+from pathlib import Path
 
 class Example(QWidget):
 
@@ -14,29 +18,32 @@ class Example(QWidget):
         self.vbox = QVBoxLayout()
         self.shotsBox = QVBoxLayout()
 
+        # STRUCTURE
+        self.structure = {
+            "Anim": [],
+            "Layout": ["mb", "ma"],
+            "Houdini": ["hip"],
+            "Nuke": ["nk"],
+            "Render": []
+        }
+
+
         # DATA
-        #self.projectsFolder = "D:\Pasha\Test"
-        self.projectsFolder = "S:"
-        self.sourceFolder = ""
+        self.inifile = 'settings.txt'
+        self.initSettings()
         self.shotDict = dict()
-
-        self.size = 50
-        self.iconScale = 0.95
-
         self.initUI()
 
-    def addShot(self):
-        newShotName = 's{0:02d}0'.format(self.lastShot+1)
-        # Create Fodler Structure
-        os.makedirs(self.sourceFolder + "/" + newShotName)
-        os.makedirs(self.sourceFolder + "/" + newShotName + "/Houdini")
-        os.makedirs(self.sourceFolder + "/" + newShotName + "/Anim")
-        os.makedirs(self.sourceFolder + "/" + newShotName + "/Render")
-        os.makedirs(self.sourceFolder + "/" + newShotName + "/Layout")
-        os.makedirs(self.sourceFolder + "/" + newShotName + "/Nuke")
+    def initSettings(self):
+        self.settings = json.load(open(self.inifile))
+        self.projectsFolder = self.settings['projectsFolder'].replace("\\\\","\\")
+        self.currProject = self.settings['currProject']
+        self.projects = self.settings['projects']
+        for key in self.projects.keys():
+            self.projects[key].replace("\\\\","\\")
 
-        addShot = QPushButton("+Add Shot")
-        self.addShotButtons(self.sourceFolder + "\\"+ newShotName +"\\")
+        self.size = int(self.settings['icon_size'])
+        self.iconScale = 0.95
 
     def on_context_menu(self, point,button,dir,ext):
         self.popMenu = QMenu(self)
@@ -63,28 +70,30 @@ class Example(QWidget):
     def addShotButtons(self,dir):
         shotBox = QHBoxLayout()
         dirName = dir.split("\\").pop(-2)
-        print("test_02")
         self.shotDict[dirName] = dir;
         # We need to check if our string matches the s000 pattern
         if len(dirName)==4 and dirName[1].isdigit():
             shotNum = int(dirName.split("s").pop())//10
             self.lastShot = max(self.lastShot,shotNum)
-            # Create Buttons
+
             # Directory Button
             dirButton = QPushButton(dirName)
             dirButton.clicked.connect(lambda state, _dir=dir: subprocess.Popen('explorer "' + _dir + '"'))
             shotBox.addWidget(dirButton)
 
-            buttonLabels = ["Anim","Layout","Houdini","Nuke","Render"]
-            extensions = {
-                            "Anim":[],
-                            "Layout":["mb","ma"],
-                            "Houdini":["hip"],
-                            "Nuke":["nk"],
-                            "Render":[]
-            }
+            # Create Preview Icon
+            previewImgPath = dir + "preview.jpg"
+            previewImgFile = Path(previewImgPath)
+            if previewImgFile.exists():
+                previewImg = QLabel()
+                pixMap = QPixmap(previewImgPath)
+                pixMap = pixMap.scaled(self.size, self.size)
+                previewImg.setPixmap(pixMap)
+                previewImg.setFixedSize(self.size, self.size)
+                shotBox.addWidget(previewImg)
 
             # create buttons and add calbacks
+            buttonLabels =  self.structure.keys()
             for buttonLabel in buttonLabels:
                 # Create button
                 button = QPushButton("")
@@ -97,7 +106,7 @@ class Example(QWidget):
 
                 # Context Menus
                 button.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-                ext = extensions[buttonLabel]
+                ext =  self.structure[buttonLabel]
                 button.customContextMenuRequested.connect(lambda point,button=button,_dir=dir + buttonLabel,_ext = ext:self.on_context_menu(point,button,_dir,_ext))
 
                 # add button to current shot
@@ -106,37 +115,22 @@ class Example(QWidget):
             # add shot to shot box
             self.shotsBox.addLayout(shotBox)
 
-    def updateShotsBox(self):
-        self.clearLayout(self.shotsBox)
-        self.lastShot = 0
-        dirs = glob(self.sourceFolder + "\\s*\\")
-        for dir in dirs:
-            self.addShotButtons(dir)
-
-
-    def createProjectComboBox(self):
-        # select project
-        self.ProjectComboBox = QComboBox();
-        dirs = glob(self.projectsFolder + "\\*\\")
-        for dir in dirs:
-            #print(dir)
-            dirName = dir.split("\\")[-2]
-            self.ProjectComboBox.addItem(dirName, dir)
-        self.ProjectComboBox.currentIndexChanged.connect(self.projectChanged)
-        self.sourceFolder = dirs[0]
-
-        self.topBar.addWidget(self.ProjectComboBox)
-
-
-    def projectChanged(self):
-        self.sourceFolder = self.ProjectComboBox.currentData()
-        self.updateShotsBox()
-
-
     def createTopBar(self):
         #Create top bar
         self.topBar = QHBoxLayout()
         self.createProjectComboBox()
+
+        # add projects button
+        addProj = QPushButton("+")
+        addProj.setFixedSize(self.size  , self.size )
+        addProj.clicked.connect(self.addProjects)
+        self.topBar.addWidget(addProj)
+
+        # remove Project Button
+        removeProj = QPushButton("-")
+        removeProj.setFixedSize(self.size, self.size)
+        removeProj.clicked.connect(self.removeProject)
+        self.topBar.addWidget(removeProj)
 
         # update button
         update = QPushButton("Update")
@@ -145,6 +139,75 @@ class Example(QWidget):
         self.topBar.addWidget(update)
 
         self.vbox.addLayout(self.topBar)
+
+    def createProjectComboBox(self):
+        # select project
+        self.ProjectComboBox = QComboBox();
+        # Fill combo box new
+        self.updateComboBox()
+
+        if self.currProject is not "":
+            self.ProjectComboBox.setCurrentIndex(list(self.projects.keys()).index(self.currProject))
+
+        self.ProjectComboBox.currentIndexChanged.connect(self.projectChanged)
+        self.topBar.addWidget(self.ProjectComboBox)
+
+    def updateComboBox(self):
+        self.ProjectComboBox.clear()
+        for key in self.projects.keys():
+             self.ProjectComboBox.addItem(key, self.projects[key])
+
+        #if self.currProject is not "":
+        #    self.ProjectComboBox.setCurrentIndex(self.projects.keys().index(self.currProject))
+
+    def projectChanged(self):
+        self.clearLayout(self.shotsBox)
+        if self.ProjectComboBox.currentText() in self.projects.keys():
+            self.currProject = self.ProjectComboBox.currentText()
+            self.saveSettings()
+            self.updateShotsBox()
+
+    def updateShotsBox(self):
+        self.clearLayout(self.shotsBox)
+        self.lastShot = 0
+        if self.currProject is not "":
+            self.sourceFolder = self.projects[self.currProject]
+            dirs = glob(self.sourceFolder + "\\s*\\")
+            for dir in dirs:
+                self.addShotButtons(dir)
+
+    def addProjects(self):
+        # Add Projects t oselectable projects
+        # Ask user to select paths
+        paths = []
+        file_dialog = QFileDialog(directory=self.projectsFolder)
+        file_dialog.setFileMode(QFileDialog.DirectoryOnly)
+        file_dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+        file_view = file_dialog.findChild(QListView, 'listView')
+
+        # to make it possible to select multiple directories:
+        if file_view:
+            file_view.setSelectionMode(QAbstractItemView.MultiSelection)
+        f_tree_view = file_dialog.findChild(QTreeView)
+        if f_tree_view:
+            f_tree_view.setSelectionMode(QAbstractItemView.MultiSelection)
+
+        if file_dialog.exec():
+            paths = file_dialog.selectedFiles()
+
+        for path in paths:
+            path = path.replace("/", "\\")
+            self.projects[path.split("\\").pop()] = path
+
+        self.saveSettings()
+        self.updateComboBox()
+
+    def removeProject(self):
+        projToRemove = self.ProjectComboBox.currentText()
+        self.currProject = ""
+        self.ProjectComboBox.removeItem(self.ProjectComboBox.currentIndex())
+        self.projects.pop(projToRemove,None)
+        self.saveSettings()
 
     def createRootBar(self):
         self.rootBar = QHBoxLayout()
@@ -164,7 +227,6 @@ class Example(QWidget):
         render.clicked.connect(lambda: subprocess.Popen('explorer "' + self.sourceFolder + 'Render"'))
         self.rootBar.addWidget(render)
         self.vbox.addLayout(self.rootBar)
-
 
     def createAssetsBar(self):
         self.assetsBar = QHBoxLayout()
@@ -196,23 +258,55 @@ class Example(QWidget):
             for folder in folders:
                 os.makedirs(assetPath + "\\" + folder)
 
+    def createBotBar(self):
+        self.botBar = QHBoxLayout()
+
+        # add shots button
+        addShot = QPushButton("+Add Shot")
+        addShot.clicked.connect(self.addShot)
+        self.botBar.addWidget(addShot)
+
+        # add shots button
+        createBase = QPushButton("CreateBase")
+        createBase.clicked.connect(self.createBase)
+        self.botBar.addWidget(createBase)
+
+        self.vbox.addLayout(self.botBar)
+
+    def addShot(self):
+        if self.currProject is not "":
+            newShotName = 's{0:02d}0'.format(self.lastShot+1)
+            # Create Fodler Structure
+            os.makedirs(self.sourceFolder + "/" + newShotName)
+            os.makedirs(self.sourceFolder + "/" + newShotName + "/Houdini")
+            os.makedirs(self.sourceFolder + "/" + newShotName + "/Anim")
+            os.makedirs(self.sourceFolder + "/" + newShotName + "/Render")
+            os.makedirs(self.sourceFolder + "/" + newShotName + "/Layout")
+            os.makedirs(self.sourceFolder + "/" + newShotName + "/Nuke")
+
+            addShot = QPushButton("+Add Shot")
+            self.addShotButtons(self.sourceFolder + "\\"+ newShotName +"\\")
+
+    def createBase(self):
+        if self.currProject is not "":
+            defaultFolders = ["Previs","Render","Assets"]
+            for dir in defaultFolders:
+                os.makedirs(self.sourceFolder + "/" + dir)
+            shutil.copy2("seafile-ignore.txt",self.sourceFolder + "/")
+
     def initUI(self):
         self.createTopBar()
         self.createRootBar()
         self.createAssetsBar()
-
 
         # shots box
         self.updateShotsBox()
         self.vbox.addLayout(self.shotsBox)
         self.vbox.addStretch()
 
-        # add shots button
-        addShot = QPushButton("+Add Shot")
-        addShot.clicked.connect(self.addShot)
-        self.vbox.addWidget(addShot)
+        self.createBotBar()
 
-        # add shots button
+        # add test button
         test = QPushButton("test")
         test.clicked.connect(self.test)
         self.vbox.addWidget(test)
@@ -224,11 +318,19 @@ class Example(QWidget):
         self.show()
 
     def test(self):
-        #(text, truth) = QInputDialog.getText(self, "Get text", "User name", QLineEdit.Normal, "NoName")
-        #if truth:
-            # The user has accepted the edit, he/she has clicked OK
-        #    print(text)
-        print(self.sourceFolder)
+        print ("test")
+
+    def saveSettings(self):
+        self.settings["projectsFolder"] = self.projectsFolder
+        projects = dict()
+        for key in self.projects.keys():
+            projects[key] = self.projects[key]
+        self.settings["projects"] = projects
+        self.settings["currProject"] = self.currProject
+
+        # write to file
+        with open(self.inifile, 'w') as outfile:
+            json.dump(self.settings, outfile)
 
     def clearLayout(self, layout):
         if layout is not None:
--- Harbor/test.py
+++ Harbor/test.py
@@ -1,21 +1,2 @@
-import sys
-from PyQt4.QtCore import Qt
-from PyQt4.QtGui import *
-
-
-class TableWidget(QTableWidget):
-    def __init__(self, parent=None):
-        QTableWidget.__init__(self, parent)
-
-    def contextMenuEvent(self, event):
-        menu = QMenu(self)
-        quitAction = menu.addAction("Quit")
-        action = menu.exec_(self.mapToGlobal(event.pos()))
-        if action == quitAction:
-            qApp.quit()
-
-
-app = QApplication([])
-tableWidget = TableWidget()
-tableWidget.show()
-sys.exit(app.exec_())
+import shutil
+shutil.copy2("seafile-ignore.txt","D:/")
