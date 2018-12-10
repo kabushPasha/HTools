ab3dd42199676416b7145f1db9f7d7904c5f2f3b
kabushPasha <kabushpavel@gmail.com>
2018-12-10 17:01:14 +0300

    tracking test

--- batch/kinect.py
+++ batch/kinect.py
@@ -20,44 +20,34 @@ def start():
 	print ("starting kinect")
 	kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Body)
 	print (kinect)
+	return kinect
 def end():
 	print ("ending kinect")
 	kinect.close()
+	print (kinect)
 def capture():
 	print ("captureing frame")
 	print (kinect.has_new_body_frame())
-	'''
-	if self._kinect.has_new_body_frame():            
-            self._bodies = self._kinect.get_last_body_frame()
-                        
-            if self._bodies is not None: 
-                for i in range(0, self._kinect.max_body_count):
-                    body = self._bodies.bodies[i]
-                    n.parm("b" + str(i)).set(body.is_tracked)
-                    if not body.is_tracked: 
-                        continue 
-                    
-                    #hou.playbar.stop()    
-                        
-                    joints = body.joints 
-                    # convert joint coordinates to color space 
-                    #joint_points = self._kinect.body_joints_to_color_space(joints)
-                    #print joints
-                    #self.draw_body(joints, joint_points, SKELETON_COLORS[i])
-                    for i in range(0,25):
-                        x = joints[i].Position.x
-                        y = joints[i].Position.y
-                        z = joints[i].Position.z
-                        
-                        n.parm("usept" + str(i)).set(1)
-                        n.parm("pt" + str(i) + "x").set(x)
-                        n.parm("pt" + str(i) + "y").set(y)
-                        n.parm("pt" + str(i) + "z").set(z)
-                        
-                        print ("updatet Joints")
-                        n.node("add1").cook()
-                        n.cook()
-	'''
+	
+	if kinect.has_new_body_frame():            
+		bodies = kinect.get_last_body_frame()
+					
+		if bodies is not None: 
+			for i in range(0, kinect.max_body_count):
+				body = bodies.bodies[i]
+				print ("body",i, "is itracked " , body.is_tracked)
+				if not body.is_tracked: 
+					continue 
+					
+				joints = body.joints 
+
+				for i in range(0,25):
+					x = joints[i].Position.x
+					y = joints[i].Position.y
+					z = joints[i].Position.z
+					
+					print(x)
+
 
 
 
@@ -69,7 +59,7 @@ while True:
 		#conn.sendall(b"Answer")
 		
 		if data == b"start":
-			start()
+			kinect = start()
 		if data == b"end":
 			end()
 		if data == b"capture":
