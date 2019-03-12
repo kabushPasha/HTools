508637db8ab715dbb31f853293f48bbebb4878ed
kabushPasha <kabushpavel@gmail.com>
2019-03-12 19:19:49 +0300

    update on crf

--- batch/lzcrf.bat
+++ batch/lzcrf.bat
@@ -3,4 +3,4 @@ set crf= %2
 
 IF "%2"=="" set crf=10
 
-ffmpeg -i %1 -c:v libx264 -preset veryslow -crf %crf% "out.mp4"
+ffmpeg -i %1 -c:v libx264 -pix_fmt yuv420p -preset veryslow -crf %crf%  -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "out.mp4"
