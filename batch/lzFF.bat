6b27f78b7a56ee791794e966cfeec77c20fa8c8a
kabushPasha <kabushpavel@gmail.com>
2019-06-24 11:29:44 +0300

    updated LZFF

--- batch/lzFF.bat
+++ batch/lzFF.bat
@@ -1,9 +1,20 @@
-::ffmpeg -i %%04d.jpg -c:v libx264 -preset veryslow -crf 0 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ../output.mp4
+echo off
 
+:: Get Startframe
 :: if start frame is not provided it will be set to 1
-
 set startframe=%1
 IF "%1"=="" set startframe=1
 ::echo %startframe%
 
+:: Get Current Directory
+for %%I in (.) do set CurrDirName=%%~nxI
+echo %CurrDirName%
+
+:: Convert Frames To Video
 ffmpeg -start_number %startframe% -framerate 24 -i %%04d.jpg -c:v libx264  -pix_fmt yuv420p -preset veryslow -crf 0 -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -r 24 "%cd%.mp4"
+
+:: Convert Video to a better format
+cd .. 
+lzcrf %CurrDirName%.mp4
+
+
--- batch/lzcrf.bat
+++ batch/lzcrf.bat
@@ -1,6 +1,7 @@
 set file=%1
 set crf= %2
+set filename=%~n1
 
 IF "%2"=="" set crf=10
 
-ffmpeg -i %1 -c:v libx264 -pix_fmt yuv420p -preset veryslow -crf %crf%  -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "out.mp4"
+ffmpeg -i %1 -c:v libx264 -pix_fmt yuv420p -preset veryslow -crf %crf%  -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "%filename%_c.mp4"
