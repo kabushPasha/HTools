21049002c12b1021ae73abb659b9c85c5e86189d
kabushPasha <kabushpavel@gmail.com>
2022-11-14 10:30:47 +0100

    updates

--- batch/FTP/download.bat
+++ batch/FTP/download.bat
@@ -3,6 +3,9 @@
 echo off
 
 ::echo %1
+::echo %2
+::echo %3
+
 ::echo %~nx2
 ::echo %~dp2
 
@@ -10,6 +13,7 @@ echo off
 set str=%2
 ::echo %str%
 set str=%str:/Fileserver/Projects/=Z:/%
+::echo %str%
 
 robocopy /S /E %~dp1 %str% *
-::pause
+pause
