a625508ce69681a306fc3d695851155653addc6f
kabushPasha <kabushpavel@gmail.com>
2022-11-28 19:18:49 +0100

    Update lzftp.py

--- HEXT/scripts/python/lzftp.py
+++ HEXT/scripts/python/lzftp.py
@@ -1,4 +1,4 @@
-import os,ftplib
+import os,ftplib,time,datetime
 
 def ftp_normpath(path):
 	return os.path.normpath(path).replace(os.sep,"/")
@@ -9,6 +9,29 @@ def ftp_login(login_str = ""):
 	login = login_str.replace(":","@").split("@")
 	print("Logging in to:",login[2])
 	return ftplib.FTP(login[2],login[0],login[1]) 
+
+def ftp_canUploadFile(ftp,local_file,ftp_filename):
+	if ftp_filename in ftp.nlst():
+		# Check timestamp
+		remote_timestamp = ftp.voidcmd(f"MDTM {ftp_filename}")[4:].strip()
+		remote_timestamp = time.mktime(time.strptime(remote_timestamp, '%Y%m%d%H%M%S'))
+		local_timestamp = time.mktime(time.gmtime(os.path.getmtime(local_file)))
+		if (local_timestamp < remote_timestamp):
+			print("Newer version on FTP")
+			return False
+	return True
+	
+def ftp_canDownloadFile(ftp,local_file,ftp_filename):
+	if os.path.isfile(local_file):
+		# Check timestamp
+		remote_timestamp = ftp.voidcmd(f"MDTM {ftp_filename}")[4:].strip()
+		remote_timestamp = time.mktime(time.strptime(remote_timestamp, '%Y%m%d%H%M%S'))
+		local_timestamp = time.mktime(time.gmtime(os.path.getmtime(local_file)))
+		if (local_timestamp >= remote_timestamp):
+			print("Newer version on Disk")
+			return False
+	return True
+	
 
 def ftp_downloadFile(login_str,local_file,ftp_file):
 	ftp = ftp_login(login_str)
@@ -21,12 +44,26 @@ def ftp_downloadFile(login_str,local_file,ftp_file):
 	ftp.cwd(ftp_dir)
 	if ftp_filename in ftp.nlst():
 		os.makedirs(os.path.dirname(local_file), exist_ok=True)
+		
+		# check Local file
+		if not ftp_canDownloadFile(ftp,local_file,ftp_filename):
+			ftp.quit()
+			return
+			
+		# download file
 		ftp.retrbinary("RETR " + ftp_filename, open(local_file, 'wb').write)
+		
+		# fix timestamp
+		remote_timestamp = ftp.voidcmd(f"MDTM {ftp_filename}")[4:].strip()
+		remote_timestamp = time.mktime(time.strptime(remote_timestamp, '%Y%m%d%H%M%S'))
+		# fix utc offset
+		remote_timestamp -= time.mktime(time.gmtime(remote_timestamp)) - remote_timestamp
+		os.utime(local_file, (remote_timestamp, remote_timestamp))		
 		print("Downloaded")
 	else:
 		print("ERROR:File not on ftp")
 	ftp.quit()
-	
+
 def ftp_uploadFile(login_str,local_file,ftp_file):
 	ftp = ftp_login(login_str)
 	local_file = ftp_normpath(local_file)
@@ -38,6 +75,12 @@ def ftp_uploadFile(login_str,local_file,ftp_file):
 	
 	# cwd and create dir
 	ftp_cwdAndCreateDir(ftp,ftp_dir)
+	
+	# Check if the file is on FTP
+	if not ftp_canUploadFile(ftp,local_file,ftp_filename):
+		ftp.quit()
+		return
+	
 	ftp.storbinary("STOR " + ftp_filename, open(local_file,'rb'))
 	print("Uploaded")
 	ftp.quit()
@@ -94,5 +137,9 @@ def ftp_uploadFolder(login_str,local_dir,ftp_dir):
 			for filename in filenames:
 				local_file = os.path.join( dirpath,filename)
 				print("Uploading: ",local_file)	
+				
+				if not ftp_canUploadFile(ftp,local_file,filename):
+					continue		
+					
 				ftp.storbinary("STOR " + filename, open(local_file,'rb'))
 	print("DONE!")
