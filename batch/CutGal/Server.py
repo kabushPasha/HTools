cacfcd82b953c9d5baf6c772656c521787fd613e
kabushPasha <kabushpavel@gmail.com>
2023-10-28 16:03:21 +0200

    updates

--- batch/CutGal/Server.py
+++ batch/CutGal/Server.py
@@ -5,14 +5,14 @@
 
 import asyncio 
 import websockets
-import glob, json, os, subprocess, sys ,random
+import glob, json, os, subprocess, sys ,random,shutil
 import urllib
 
 if "o" in sys.argv:
 	html_file = os.path.dirname(__file__) + "\TestGrid.html" 
 	os.system(html_file.replace("\\","/") )
 
-src_folder = sys.argv[1]
+src_folder = sys.argv[1].replace(os.sep,"/")
 
 class CanoeAmbientServer():
 	def __init__(self):	
@@ -23,12 +23,18 @@ class CanoeAmbientServer():
 		while True:
 			msg = await websocket.recv()
 			msg = json.loads(msg)
+			print(msg)
 			
 			if msg[0] == 'GET_Files':
 				files = glob.glob( src_folder + "/*.json" )	
 				all_items = []
 				for file in files:					
-					all_items.append(json.load(open(file)))				
+					json_data = json.load(open(file))
+					# change abs paths to relative
+					json_data[0] = src_folder +"/" +os.path.basename(json_data[0])
+					json_data[1] = src_folder +"/" +os.path.basename(json_data[1])
+					all_items.append(json_data)						
+					
 				await websocket.send(json.dumps(all_items))
 			if msg[0] == "save":
 				print("save", msg[1])
@@ -41,7 +47,15 @@ class CanoeAmbientServer():
 				cmd = f'ffmpeg -ss {msg[1][1]} -to {msg[1][2]} -i "{msg[1][0]}" -c copy "{out_file}"'
 				print(cmd)
 				subprocess.Popen(cmd)
-
+			if msg[0] == "move":
+				print("move", msg[1])				
+				out_dir = os.path.dirname(msg[1]) + "/Done/"
+				if not os.path.isdir(out_dir) :  os.mkdir(out_dir)
+				
+				files = glob.glob( os.path.splitext(msg[1])[0] + "*" )	
+				for file in files:				
+					shutil.move( file,out_dir );
+				
 
 	def startServer(self):
 		start_server = websockets.serve(self.serve, "localhost", 8000)
