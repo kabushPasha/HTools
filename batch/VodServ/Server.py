0b4e1d0c647d1c152e86a0834d9f88e3bbf2f6ed
kabushPasha <kabushpavel@gmail.com>
2023-12-28 22:50:55 +0100

    updates

--- batch/VodServ/Server.py
+++ batch/VodServ/Server.py
@@ -8,20 +8,24 @@ import websockets
 import glob, json, os, subprocess, sys ,random,time
 import urllib
 
-if "o" in sys.argv:
+
+
+#if "o" in sys.argv:
+if True:
 	html_file = os.path.dirname(__file__) + "\client.html" 
 	#os.system(html_file.replace("\\","/") )
+	
+	if "m" in sys.argv:
+		html_file = os.path.dirname(__file__) + "\client_Multi.html" 
+		#os.system(html_file.replace("\\","/") )
 	
 	page = "file:///" + html_file.replace("\\","/")
 	open_command = r'start chrome --profile-directory="Default" '
 	open_command += r'--app=' + page	
 	os.system(open_command)
 	
-if "m" in sys.argv:
-	html_file = os.path.dirname(__file__) + "\client_Multi.html" 
-	os.system(html_file.replace("\\","/") )
-
-src_folder = sys.argv[1]
+	
+src_folder = os.path.normpath(sys.argv[1])
 
 files = sorted(glob.glob( src_folder + "/*.mp4" ) + glob.glob( src_folder + "/*.mkv" ),reverse=True)
 if "r" in sys.argv: random.shuffle(files)
@@ -42,15 +46,23 @@ class CanoeAmbientServer():
 				await websocket.send(json.dumps(files))
 			if msg[0] == "delete":
 				print("delete", msg[1])
-				current_file = msg[1].replace("file:///","")
-				os.remove( urllib.parse.unquote(current_file) )
+				current_file = urllib.parse.unquote(msg[1].replace("file:///",""))
+				current_file = os.path.normpath(current_file)
+				time.sleep(0.1)			
+
+				if os.path.dirname(current_file) == src_folder:	
+					os.remove( current_file )
 			if msg[0] == "save":
 				print("save", msg[1])
 				src_path = urllib.parse.unquote(msg[1].replace("file:///",""))
+				src_path = os.path.normpath(src_path)
 				dest_path = os.path.dirname(src_path) + "/picked/" + os.path.basename(src_path)
 				if not os.path.isdir(os.path.dirname(dest_path)): os.mkdir(os.path.dirname(dest_path))
 				time.sleep(0.1)
-				os.rename(src_path, dest_path)
+				
+				# move if the file is in the same folder
+				if os.path.dirname(src_path) == src_folder:				
+					os.rename(src_path, dest_path)
 
 	def startServer(self):
 		start_server = websockets.serve(self.serve, "localhost", 8000)
