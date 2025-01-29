d9244162064f6e0378d098e96baea3157b53105a
kabushPasha <kabushpavel@gmail.com>
2025-01-29 18:13:19 +0100

    updates

--- batch/ML_PythonScripts/hu3d_folder.py
+++ batch/ML_PythonScripts/hu3d_folder.py
@@ -5,39 +5,51 @@ from PIL import Image
 
 from hy3dgen.rembg import BackgroundRemover
 from hy3dgen.shapegen import Hunyuan3DDiTFlowMatchingPipeline, FaceReducer, FloaterRemover, DegenerateFaceRemover
-from hy3dgen.text2image import HunyuanDiTPipeline
 
+from mmgp import offload
+
+from hy3dgen.texgen import Hunyuan3DPaintPipeline
+model_path = 'tencent/Hunyuan3D-2'
+texgen_worker = Hunyuan3DPaintPipeline.from_pretrained(model_path)
+i23d_worker = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(model_path)
+
+pipe = offload.extract_models("i23d_worker", i23d_worker)
+pipe.update(  offload.extract_models( "texgen_worker", texgen_worker))
+
+kwargs = {}
+kwargs["pinnedMemory"] = "i23d_worker/model"
+offload.profile(pipe, profile_no = 3, verboseLevel = int(1), **kwargs)
+  
 
 def image_to_3d(image_path='assets/demo.png'):
-    rembg = BackgroundRemover()
-    model_path = 'tencent/Hunyuan3D-2'
+	rembg = BackgroundRemover()
+	
 
-    image = Image.open(image_path)
-    image = image.resize((1024, 1024))
+	image = Image.open(image_path)
+	#image = image.resize((1024, 1024))
 
-    if image.mode == 'RGB':
-        image = rembg(image)
+	if image.mode == 'RGB':
+		image = rembg(image)
 
-    pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(model_path)
+	print("Processing Mesh")
+	mesh = i23d_worker(image=image, num_inference_steps=30, mc_algo='mc',
+					generator=torch.manual_seed(2025))[0]
+	print("MESH GENERATED")
+	mesh = FloaterRemover()(mesh)
+	mesh = DegenerateFaceRemover()(mesh)
+	mesh = FaceReducer()(mesh)
+	#mesh.export("out/" + os.path.basename(image_path) + 'mesh.glb')
+	print("MESH EXPORTED")
+	
+	print("Pipeline loaded, Texturing")
+	textured_mesh = texgen_worker(mesh, image)
+	print("Exporting Mesh")
+	textured_mesh.export( "out/" + os.path.basename(image_path) + '.glb',include_normals=True)
+	textured_mesh.export( image_path + '.glb',include_normals=True)
 
-    mesh = pipeline(image=image, num_inference_steps=30, mc_algo='mc',
-                    generator=torch.manual_seed(2025))[0]
-    mesh = FloaterRemover()(mesh)
-    mesh = DegenerateFaceRemover()(mesh)
-    mesh = FaceReducer()(mesh)
-    #mesh.export('mesh.glb')
-
-    try:
-        from hy3dgen.texgen import Hunyuan3DPaintPipeline
-        pipeline = Hunyuan3DPaintPipeline.from_pretrained(model_path)
-        mesh = pipeline(mesh, image=image)
-        mesh.export( image_path + '.glb')
-    except Exception as e:
-        print(e)
-        print('Please try to install requirements by following README.md')
 
 if __name__ == '__main__':
-    #image_to_3d()
+	#image_to_3d()
 	print("CONVERTING FOLDER:")
 	print(os.environ["SRC_DIR"])
 	print("IMAGES:")
@@ -49,5 +61,5 @@ if __name__ == '__main__':
 		image_to_3d(image_path = img)
 		
 		
-	
+
 	
